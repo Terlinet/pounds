0f7b295e4c3c96e1ee53441027f0b42d2dd14aec
Tertuliano Barbosa <arazaodetudoeoamor@gmail.com>
2026-05-29 08:55:54 -0300

    Built web
    Branch: master
    Commit: c2c4d7d0ada55db2a33820654a356583cdaeca7b (dirty)
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1680677755" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2766255854" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
